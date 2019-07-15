2fbcb7fab309dce37c90e383637bffae15f11bd1
KagaK2 <KhanhPhan2512@gmail.com>
2019-07-15 14:19:09 +0300

    2.11 done

--- part2/thephonebook/src/App.js
+++ part2/thephonebook/src/App.js
@@ -1,19 +1,19 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 import Filter from "./components/Filter";
 import PersonForm from "./components/PersonForm";
 import Persons from "./components/Persons";
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: "Arto Hellas", number: "040-123456" },
-    { name: "Ada Lovelace", number: "39-44-5323523" },
-    { name: "Dan Abramov", number: "12-43-234345" },
-    { name: "Mary Poppendieck", number: "39-23-6423122" }
-  ]);
+  const [persons, setPersons] = useState([]);
   const [newName, setNewName] = useState("");
   const [newNumber, setNewNumber] = useState("");
   const [filter, setFilter] = useState("");
-
+  useEffect(() => {
+    axios.get("http://localhost:3001/persons").then(response => {
+      setPersons(response.data);
+    });
+  }, []);
   const onFormSubmit = () => {
     const filteredArray = persons.filter(person => person.name === newName);
     if (filteredArray.length === 0) {
@@ -24,19 +24,26 @@ const App = () => {
     setNewName("");
     setNewNumber("");
   };
+
   return (
     <div>
-      <h2>Phonebook</h2>
-      <Filter onChange={setFilter} filter={filter} />
-      <PersonForm
-        onNameChange={setNewName}
-        onNumberChange={setNewNumber}
-        newName={newName}
-        newNumber={newNumber}
-        onFormSubmit={onFormSubmit}
-      />
-      <h2>Numbers</h2>
-      <Persons persons={persons} filter={filter} />
+      {persons.length > 0 ? (
+        <div>
+          <h2>Phonebook</h2>
+          <Filter onChange={setFilter} filter={filter} />
+          <PersonForm
+            onNameChange={setNewName}
+            onNumberChange={setNewNumber}
+            newName={newName}
+            newNumber={newNumber}
+            onFormSubmit={onFormSubmit}
+          />
+          <h2>Numbers</h2>
+          <Persons persons={persons} filter={filter} />
+        </div>
+      ) : (
+        <div>Loading</div>
+      )}
     </div>
   );
 };
